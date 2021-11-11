dd06adc441c6c8be7fd83eba5f50ca4aa443fac1
email <abe.solis2015@gmail.com>
2021-11-11 01:20:44 -0800

    redo

--- script.js
+++ script.js
@@ -4,21 +4,29 @@ document.getElementById('generate').addEventListener('click', () => {
 
 // User Inputs
 
- let passLength = prompt("Enter desired password between 8 - 128 charcters long...");
-
- let lowerList = confirm("Would you like Lowercase characters? OK or CANCEL?");
+let lowerList = confirm("Would you like Lowercase characters? OK or CANCEL?");
  
 
- let upperList = confirm("Would you like Uppercase characters? OK or CANCEL?");
+let upperList = confirm("Would you like Uppercase characters? OK or CANCEL?");
 
 
- let specialList = confirm("Would you like special characters? OK or CANCEL?");
+let specialList = confirm("Would you like special characters? OK or CANCEL?");
+
+
+let numList = confirm('Would you like numbers? OK or CANCEL?');
+
+//  Valadating User Inputs
+
+if (!lowerList && !upperList && !specialList && !numList){
+  alert("Refresh the page and again....");
+  return;
+};
+
+// Valading User Length Request
+let passLength = prompt("Enter desired password between 8 - 128 charcters long...");
 
 
 
-let numList = confirm('Would you like numbers? OK or CANCEL?')
-
- 
 })
 
 
