ea5d3dcfd16c567826112736daf34b41bd4e6192
email <abe.solis2015@gmail.com>
2021-11-10 20:24:05 -0800

    prompts

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const alphaList = 'abcdefghijklmnopqrstuvwxyz'
+
+const numList = '1234567890'
+
+const specialList = '!@#$%^&*()'
+
+const code = []
+
+
+
+
 document.getElementById('generate').addEventListener('click', () => {
+
+  prompt("Enter desired password between 8 - 128 charcters long...")
+  prompt("Would you like Lowecase characters? Yes or No?")
+  prompt("Would you like Uppercase characters? Yes or No?")
+  prompt("Would you like special characters? Yes or No?")
+
+
+
 
 })
