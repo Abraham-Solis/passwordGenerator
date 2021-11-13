94c32e99db757c0ef74c05f49bcb443bc85ce4bc
email <abe.solis2015@gmail.com>
2021-11-12 20:14:58 -0800

    overhaul

--- script.js
+++ script.js
@@ -1,91 +1,116 @@
-document.getElementById('generate').addEventListener('click', () => {
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 
+let choices;
+let toUpper = function (a) {
+  return a.toUpperCase();
+};
+upperCase = lowerCase.map(toUpper);
 
-  // User Inputs
+let enter;
+let confirmsymbols;
+let confirmUppercase;
+let confirmNumber; 
+let confirmLowercase; 
 
-  let lowerCase = confirm("Would you like Lowercase characters? OK or CANCEL?");
+function generatePassword() {
 
+  enter = parseInt(prompt("Please enter a desired amount of between 8-128."));
 
-  let upperCase = confirm("Would you like Uppercase characters? OK or CANCEL?");
+  if (!enter) {
+    alert("PLEASE ENTER ACCORDING TO PROMPT!");
+  } else if (enter < 8 || enter > 128) {
 
+    enter = parseInt(prompt("Please stay between 8 and 128"));
 
-  let specialList = confirm("Would you like special characters? OK or CANCEL?");
+  } else {
 
-
-  let numList = confirm('Would you like numbers? OK or CANCEL?');
-
-  //  Valadating User Inputs
-
-  if (!lowerCase && !upperCase && !specialList && !numList) {
-    alert("Refresh the page and again....");
-    return;
+    confirmsymbols = confirm("Do u want symbols?");
+    confirmUppercase = confirm("Do u want UpperCase Letters?");
+    confirmLowercase = confirm("Do u want LowerCase Letters?");
   };
 
-  // Valading User Length Request
-  let passLength = prompt("Enter desired password between 8 - 128 characters long...");
 
-  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-    alert("Please Select a valid number 8 and 128");
-    return;
+  if (!confirmsymbols && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+    choices = alert("WHY U NO PICK ONE>>REFRESH");
 
-  };
-
-  var choices = ""
-
-  if (lowerCase) {
-  choices += lowerAlph
-}
-
-if (upperCase) {
-  choices += upperAlph
-  
-}
-if (specialList) {
-  choices += symbols
-}
-
-  if (numList) {
-  choices += numerical
-  
-}
-
-var newPassword = "";
-
-
-  let userChoices = "";
-
-  for (i = 0; i < passLength; i++) {
-    Userchoices = choices[Math.floor(Math.random() * choices.length)];
-    // newPassword.push(Userchoices);
   }
 
+  else if (confirmsymbols && confirmNumber && confirmUppercase && confirmLowercase) {
 
-  var ps = newPassword.join("");
+    choices = symbols.concat(number, lowerCase, upperCase);
+  }
+  else if (confirmsymbols && confirmNumber && confirmLowercase) {
+    choices = symbols.concat(number, lowerCase); 
+  }
+  else if (confirmNumber && confirmLowercase && confirmUppercase) {
+    choices = number.concat(lowerCase, upperCase); 
+  }
+  else if (confirmsymbols && confirmNumber && confirmUppercase) {
+    choices = symbols.concat(number, upperCase);
+  }
+  else if (confirmsymbols && confirmLowercase && confirmUppercase) {
+    choices = symbols.concat(lowerCase, upperCase); 
+  }
+
+  else if (confirmsymbols && confirmNumber) {
+    choices = symbols.concat(number); 
+
+  } else if (confirmNumber && confirmUppercase) {
+    choices = number.concat(upperCase); 
+
+  } else if (confirmsymbols && confirmUppercase) {
+    choices = symbols.concat(upperCase);
+  }
+    else if (confirmLowercase && confirmNumber) {
+    choices = lowerCase.concat(number);
+
+  } else if (confirmLowercase && confirmUppercase) {
+    choices = lowerCase.concat(upperCase);
+  
+  } else if (confirmsymbols && confirmLowercase) {
+  choices = symbols.concat(lowerCase); 
+  
+  }else if (confirmLowercase) {
+    choices = lowerCase;
+  
+  }else if (confirmsymbols) {
+    choices = symbols; 
+  
+  }else if (confirmNumber) {
+    choices = number;
+
+  }else if (confirmUppercase) {
+    choices = space.concat(upperCase);
+
+  };
+
+  let password = [];
+
+
+  for (let i = 0; i < enter; i++) {
+    let pickChoices = choices[Math.floor(Math.random() * choices.length)];
+    password.push(pickChoices);
+  }
+
+  let ps = password.join("");
   UserInput(ps);
   return ps;
+}
 
+function UserInput(ps) {
+  document.getElementById("password").textContent = ps;
 
-})
+}
 
+let get = document.querySelector("#generate");
 
-
-
-  function UserInput(ps) {
-    document.getElementById("password").textContent = ps;
-  }
-
-  document.getElementById("click", function () {
-    ps = generatePassword();
-    document.getElementById("password").placeholder = ps;
-  });
-
-
-
-
-const lowerAlph = 'defghijklmnopqrstuvwxyz';
-const upperAlph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const numerical = '1234567890';
-const symbols = '!@#$%^&*()_+|}{":?><,./;][';
-
+get.addEventListener("click", function () {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
 
