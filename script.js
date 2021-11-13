42875ca829eebd29c7d60d53cf81bb3c86bc2ba6
email <abe.solis2015@gmail.com>
2021-11-12 17:32:21 -0800

    code

--- script.js
+++ script.js
@@ -2,100 +2,85 @@ document.getElementById('generate').addEventListener('click', () => {
 
 
 
-// User Inputs
+  // User Inputs
 
-let lowerList = confirm("Would you like Lowercase characters? OK or CANCEL?");
- 
-
-let upperList = confirm("Would you like Uppercase characters? OK or CANCEL?");
+  let lowerCase = confirm("Would you like Lowercase characters? OK or CANCEL?");
 
 
-let specialList = confirm("Would you like special characters? OK or CANCEL?");
+  let upperCase = confirm("Would you like Uppercase characters? OK or CANCEL?");
 
 
-let numList = confirm('Would you like numbers? OK or CANCEL?');
+  let specialList = confirm("Would you like special characters? OK or CANCEL?");
 
-//  Valadating User Inputs
 
-if (!lowerList && !upperList && !specialList && !numList){
-  alert("Refresh the page and again....");
-  return;
-};
+  let numList = confirm('Would you like numbers? OK or CANCEL?');
 
-// Valading User Length Request
-let passLength = prompt("Enter desired password between 8 - 128 charcters long...");
+  //  Valadating User Inputs
 
-if(passLength <8 ||passLength <128 ||isNaN(passLength)){
-  alert("Please Select a valid number 8 and 128");
-  return;
+  if (!lowerCase && !upperCase && !specialList && !numList) {
+    alert("Refresh the page and again....");
+    return;
+  };
 
-}
+  // Valading User Length Request
+  let passLength = prompt("Enter desired password between 8 - 128 characters long...");
 
-//storing arrays that user chose
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    alert("Please Select a valid number 8 and 128");
+    return;
 
-let inputChar =[]; 
+  };
+
+
+
+
+
+
+
+let inputChar = [];
 
 //sorting array to confirm user input
 
-let guaranteedInput = [];
+let guaranteedInput = []; 
 
 
 
-  if (lowerList) {
-    inputChar = inputChar.concat(lowerCase);
-    guaranteedInput.push(
-      lowerCase[
-        Math.floor(Math.random() * lowerCase.length)]
-      console.log(lowerList)
-    );
-  }
+if (lowerCase) {
+  inputChar = inputChar.concat(lowerCase)
+  guaranteedInput.push(lowerCase[Math.floor(Math.random() * lowerCase.length)])
+  console.log(inputChar)
+}
 
 
 
+if (upperCase) {
+  inputChar = inputChar.concat(upperCase)
+  guaranteedInput.push(upperCase[Math.floor(Math.random() * upperCase.length)])
+  console.log(inputChar)
+}
 
+if (numList) {
+  inputChar = inputChar.concat(numList)
+  guaranteedInput.push(numList[Math.floor(Math.random() * numList.length)])
+  console.log(inputChar)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (specialList) {
+  inputChar = inputChar.concat(specialList)
+  guaranteedInput.push(specialList[Math.floor(Math.random() * specialList.length)])
+  console.log(inputChar)
+}
 
 
 
 })
 
-
-
-
-
-
-
-
+console.log(inputChar)
 
 
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
-const upperCase ='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const numList = '1234567890'
-const specialList ='!@#$%^&*()_+|}{":?><,./;]['
+const specialList = '!@#$%^&*()_+|}{":?><,./;]['
+
 
